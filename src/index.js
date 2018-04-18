f4184dcb57ff9874d51d8f723b63854c585b58d9
tony <tony@tonydeMacBook-Pro.local>
2018-04-18 14:09:16 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import en_US from './i18n/en_US.js';
 import zh_CN from './i18n/zh_CN.js';
 import Card from './components/card/index.vue';
-import RHeader from './components/header/index.vue';
+import XHeader from './components/header/index.vue';
 import TabBar from './components/tabBar/index.vue';
 import Actionsheet from './components/actionsheet/index.vue';
 import Drawer from './components/drawer/index.vue';
@@ -10,21 +10,21 @@ import Tab from './components/tab/tab.vue';
 import Page from './components/page/index.vue';
 import Grid from './components/grid/index.vue';
 import Scroller from './components/scroller/index.vue';
-import RImage from './components/image/index.vue';
+import XImage from './components/image/index.vue';
 import Divider from './components/divider/index.vue';
 import DateTime from './components/datetime/index.vue';
 import Selector from './components/selector/index.vue';
 import Picker from './components/picker/index.vue';
-import RTable from './components/table/index.vue';
+import XTable from './components/table/index.vue';
 import List from './components/list/index.vue';
 import Cell from './components/cell/index.vue';
 import Box from './components/box/index.vue';
 import RButton from './components/button/index.vue';
 import Swiper from './components/swiper/swiper.vue';
 import SwiperItem from './components/swiper/swiper-item.vue';
-import RSwitch from './components/switch/index.vue';
-import RInput from './components/input/index.vue';
-import RTextarea from './components/textarea/index.vue';
+import XSwitch from './components/switch/index.vue';
+import XInput from './components/input/index.vue';
+import XTextarea from './components/textarea/index.vue';
 import Toast from './components/toast/index.vue';
 import Panel from './components/panel/index.vue';
 import Row from './components/row/index.vue';
@@ -32,7 +32,7 @@ import Popup from './components/popup/index.vue';
 import Step from './components/step/index.vue';
 import Checker from './components/checker/index.vue';
 import Spinner from './components/spinner/index.vue';
-import RDialog from './components/dialog/index.vue';
+import XDialog from './components/dialog/index.vue';
 import Alert from './components/alert/index.vue';
 import AlertApi from './components/alert/api';
 import Confirm from './components/confirm/index.vue';
@@ -40,7 +40,7 @@ import LoadingApi from './components/loading/api';
 import ConfirmApi from './components/confirm/api';
 import Marquee from './components/marquee/index.vue';
 import Badge from './components/badge/index.vue';
-import RBody from './components/body/index.vue';
+import XBody from './components/body/index.vue';
 
 export {
     Card,
@@ -76,7 +76,7 @@ export {
     Step,
     Checker,
     Spinner,
-    RDialog,
+    XDialog,
     Alert,
     AlertApi,
     Confirm,
